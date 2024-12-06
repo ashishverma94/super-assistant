2bbaabda0b65e3703a7e54011761e3caa398af93
ashishverma94 <hariashish1@gmail.com>
2024-12-06 17:10:21 +0530

    commit 3

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 // import HomePage from "./pages/HomePage.jsx";
 // import QuestionsPage from "./pages/QuestionsPage";
 import CreateQuestions from "./pages/CreateQuestions";
-// import QuestionsListPage from "./pages/QuestionsListPage";
+import QuestionsListPage from "./pages/QuestionsListPage";
 import { ToastContainer } from "react-toastify";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -14,7 +14,7 @@ function App() {
         <Routes>
           {/* <Route index element={<HomePage />} /> */}
           <Route path="/create-questions" element={<CreateQuestions />} />
-          {/* <Route path="/solve-questions" element={<QuestionsListPage />} /> */}
+          <Route path="/solve-questions" element={<QuestionsListPage />} />
           {/* <Route path="/solve-questions/:id" element={<QuestionsPage />} /> */}
 
           <Route
