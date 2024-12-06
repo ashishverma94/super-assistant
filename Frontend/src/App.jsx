8b64e402be673e0ce9e8eeaab2abc914419208f4
ashishverma94 <hariashish1@gmail.com>
2024-12-06 17:23:45 +0530

    commit 7

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HomePage from "./pages/HomePage";
+import HomePage from "./Pages/HomePage";
 // import QuestionsPage from "./pages/QuestionsPage";
 // import CreateQuestions from "./pages/CreateQuestions";
 // import QuestionsListPage from "./pages/QuestionsListPage";
