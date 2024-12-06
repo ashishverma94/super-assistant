46eabf030edae84754d3bd7bf8f790bc94c24457
ashishverma94 <hariashish1@gmail.com>
2024-12-06 16:59:11 +0530

    updated app.jsx file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import {
   QuestionsPage,
   CreateQuestions,
   QuestionsListPage,
-} from "./pages/index.js";
+} from "./pages";
 import { ToastContainer } from "react-toastify";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
